788a32e365c6a51576e94efec956d6a59cdd2b4b
Luc Jaubert <lucjaubert@gmail.com>
2023-03-15 16:12:35 +0100

    challenge 1 et 2 ok

--- src/test/java/dev/wcs/nad/tariffmanager/address/AddressJdbcTest.java
+++ src/test/java/dev/wcs/nad/tariffmanager/address/AddressJdbcTest.java
@@ -1,9 +1,7 @@
 package dev.wcs.nad.tariffmanager.address;
 
-import dev.wcs.nad.tariffmanager.customer.model.Customer;
 import dev.wcs.nad.tariffmanager.persistence.entity.Address;
 import dev.wcs.nad.tariffmanager.persistence.jdbc.AddressLegacyDao;
-import dev.wcs.nad.tariffmanager.persistence.jdbc.CustomerLegacyDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 public class AddressJdbcTest {
@@ -24,6 +23,19 @@ public class AddressJdbcTest {
         assertThat(firstAddress.isEmpty()).isFalse();
         assertThat(firstAddress.get().getId().toString().equals("1")).isTrue();
     }
+
+    @Test
+    public void shouldLoadAddressWithId7() {
+        Optional<Address> address = addressLegacyDao.getByIdJava7Syntax(7);
+        assertThat(address.isEmpty()).isFalse();
+        assertThat(address.get().getId().toString().equals("7")).isTrue();
+    }
+
+    @Test
+    public void shouldReturnEmptyIfAddressNotFound() {
+        Optional<Address> address = addressLegacyDao.getByIdJava7Syntax(49);
+        assertThat(address.isEmpty()).isTrue();
+}
 
 
 }
